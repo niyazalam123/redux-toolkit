716ee86bf153c1579a7630225c89f8d42c29ceb3
Md Niyaz Alam <alamniyaz173@gmail.com>
2024-03-27 23:44:21 +0530

    code updated in slice file localstorage problem solved

--- src/app/reduxtoolkit/todoSlice.js
+++ src/app/reduxtoolkit/todoSlice.js
@@ -1,43 +1,53 @@
-import { createSlice,nanoid,current, createAsyncThunk } from "@reduxjs/toolkit"
+import { createSlice, nanoid, current, createAsyncThunk } from "@reduxjs/toolkit"
 
-const initialState = {
-    todoUsers:JSON.parse(localStorage.getItem("TodoList")) || []
+// retrive data from local storage  becuase localstorage is  not defined in server side it is only present inside client side
+const getFromLocalStorage = (key) => {
+    if (!key || typeof window === 'undefined') {
+        return ""
+    }
+    return localStorage.getItem(key)
 }
 
 
-export const fetchApiData = createAsyncThunk("fetchApiData",async()=>{
+// and we call getFromLocalStorage function and pass loaclstorage key 
+const initialState = {
+    todoUsers: getFromLocalStorage("TodoList") ? JSON.parse(getFromLocalStorage("TodoList") || {}) : [],
+}
+
+
+export const fetchApiData = createAsyncThunk("fetchApiData", async () => {
     const data2 = await fetch("https://jsonplaceholder.typicode.com/users");
     return data2.json();
 })
 
 
 const todoSlices = createSlice({
-    name:"TodoData",
+    name: "TodoData",
     initialState,
-    reducers:{
-        addTodo:(state,action)=>{
+    reducers: {
+        addTodo: (state, action) => {
             const data = {
-                id:nanoid(),
-                name:action.payload
+                id: nanoid(),
+                name: action.payload
             };
             state.todoUsers.push(data);
             const savedData = JSON.stringify(current(state.todoUsers));
-            localStorage.setItem("TodoList",savedData);
+            localStorage.setItem("TodoList", savedData);
         },
-        removeTodo:(state,action)=>{
-            const updatedTodo = state.todoUsers.filter((item)=>item.id!==action.payload);
+        removeTodo: (state, action) => {
+            const updatedTodo = state.todoUsers.filter((item) => item.id !== action.payload);
             state.todoUsers = updatedTodo;
             const savedData = JSON.stringify(updatedTodo);
-            localStorage.setItem("TodoList",savedData);
+            localStorage.setItem("TodoList", savedData);
         }
 
     },
-    extraReducers:(builder)=>{
-        builder.addCase(fetchApiData.fulfilled,(state,action)=>{
-            
+    extraReducers: (builder) => {
+        builder.addCase(fetchApiData.fulfilled, (state, action) => {
+
         })
     }
 });
 
-export const {addTodo,removeTodo}  = todoSlices.actions;
+export const { addTodo, removeTodo } = todoSlices.actions;
 export default todoSlices.reducer;
